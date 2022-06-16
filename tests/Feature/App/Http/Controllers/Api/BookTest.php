0e3fcb42f9e62b87dc8173a6a773477f5683d7d1
Viniciusli <viniciusleeg@outlook.com>
2022-06-16 10:35:29 -0300

    book test

---
+++ tests/Feature/App/Http/Controllers/Api/BookTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Tests\Feature\App\Http\Controllers\Api;
+
+use App\Models\Book;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class BookTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testBooksCanBeListedForApi(): void
+    {
+        // prepare
+        Book::factory()->create();
+
+        // act
+        $response = $this->get('/api/book');
+
+        // assert
+        $response->assertStatus(302);
+    }
+
+    public function testBookCanBeCreatedForApi(): void
+    {
+        // prepare
+        $payload = [
+            'title' => 'title test',
+            'author' => 'author test',
+            'decription' => 'description test',
+            'published_at' => '2020-01-01'
+        ];
+
+        // act
+        $response = $this->get('/api/book', $payload);
+
+        // assert
+        $response->assertStatus(201);
+        $response->seeInDatabase('books', $payload);
+    }
+
+    public function testBookCanBeUpdatedForApi(): void
+    {
+        // prepare
+        $payload = [
+            'title' => 'title test',
+            'author' => 'author test',
+            'description' => 'description test',
+            'published_at' => '2020-01-01'
+        ];
+        Book::factory()->create($payload);
+
+        // act
+        $response = $this->put('/book', [
+            'author' => 'author test updated',
+        ]);
+
+        // assert
+        $response->assertStatus(200);
+        $response->seeInDatabse('books', [
+            'author' => 'author test updated',
+        ]);
+    }
+
+    public function testUserCanBeDeletedForApi(): void
+    {
+        // prepare
+        $book = Book::factory()->create();
+
+        // act
+        $response = $this->delete('/api/book/' . $book);
+
+        // assert
+        $response->assertStatus(200);
+    }
+}
